70b36122e69044afe9aa1ed240cafa6bc36c3bf3
Mike <mi5hqaaaaov@gmail.com>
2021-02-18 15:22:30 +0200

    modalComponent

--- src/js/main.js
+++ src/js/main.js
@@ -12,17 +12,10 @@ $('button').eq(2).on('click',() => {
    $('.w-500').fadeToggle(800);
 });
 
-$('.wrap').html(
-   `
-   <div class="dropdown">
-         <button class="btn btn-outline-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
-            <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
-                  <a href="#" class="dropdown-item">Action#1</a>
-                  <a href="#" class="dropdown-item">Action#2</a>
-                  <a href="#" class="dropdown-item">Action#3</a>
-            </div>
-   </div>
-   `
-);
+// $('.wrap').html(
+//    `
+//space for markup(html)
+//    `
+// );
 
-$('.dropdown-toggle').dropdown();
+// $('.dropdown-toggle').dropdown();
